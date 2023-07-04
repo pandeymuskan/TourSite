88925909b0186ffa3538a8f211da478ab3967fee
Muskan Pandey <bit19it56@bit.ac.in>
2023-07-04 10:23:49 +0530

    first commit

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./index.css";
+const Loading = () => {
+  return (
+    <div className="loading">
+      <h1>loading...</h1>
+    </div>
+  );
+};
+export default Loading;
